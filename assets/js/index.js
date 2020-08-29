7a559fe916bdfcfaa559c2888a3f1151d9128936
flawn <flawnsontong1@gmail.com>
2020-08-28 22:59:45 -0400

    Continued work on preloader jQuery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,14 @@
-var preloader = document.getElementById('loader');
-function preLoaderHandler(){
-    preloader.style.display = 'none';
-}
+// var preloader = document.getElementById('loader');
+// function preLoaderHandler(){
+//     preloader.style.display = 'none';
+// }
+
+$(document).ready(function() {
+//Preloader
+    preloaderFadeOutTime = 500;
+    function hidePreloader() {
+        var preloader = $('.loader');
+        preloader.fadeOut(preloaderFadeOutTime);
+    }
+    hidePreloader();
+});
